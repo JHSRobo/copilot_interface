7cc0dfd41e9b0579ac343b113b449dbc7de75612
Andrew Grindstaff <ajgrinds03@gmail.com>
2022-05-07 11:08:28 -0700

    Update ops_interface.py

--- src/ops_interface.py
+++ src/ops_interface.py
@@ -4,26 +4,27 @@ from std_msgs.msg import String
 from std_msgs.msg import Bool
 
 from dynamic_reconfigure.server import Server
-from copilot_interface.cfg import opsInterfaceConfig
+from copilot_interface.cfg import copilotControlParamsConfig
+#from copilot_interface.cfg import opsInterfaceConfig
 
 lengthProgram = False
 
 def main():
     global pubLength
-    rospy.init_node('opsInterface')
+    rospy.init_node('ops_interface')
   
     pubLength = rospy.Publisher('ops/measure_toggle', Bool, queue_size=1)
     
     def opsCallback(config, level):
       global lengthProgram
 
-      lengthProgram = config.lengthFinder
+      lengthProgram = config.l_scale
       pubLength.publish(lengthProgram)
       
       
 
     # setup dynamic reconfigure
-    server = Server(opsInterfaceConfig, opsCallback)
+    server = Server(copilotControlParamsConfig, opsCallback)
     
     rospy.spin()
         
