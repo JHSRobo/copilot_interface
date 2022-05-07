230bd8e75cf5d91fdd044426302ec8a011451fad
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2022-05-07 14:24:51 -0700

    shipwreck mapper button
    code has yet to be made to go with it

--- src/ops_interface.py
+++ src/ops_interface.py
@@ -11,7 +11,7 @@ lengthProgram = False
 photomosaicProgram = False
 
 def main():
-    global pubLength, pubLasers, pubPhotomosaic
+    global pubLength, pubLasers, pubPhotomosaic, pubShipwreck
     rospy.init_node('ops_interface')
     
     pubLasers = rospy.Publisher('ops/toggle_lasers', Bool, queue_size=1)
@@ -19,19 +19,22 @@ def main():
     pubPhotomosaic = rospy.Publisher('ops/photomosaic_toggle', Bool, queue_size=1)
     
     def opsCallback(config, level):
-      global lengthProgram, toggleLasers, photomosaicProgram
+      global lengthProgram, toggleLasers, photomosaicProgram, mapShipwreck
       
       toggleLasers = config.toggle_lasers
       lengthProgram = config.length_finder
       photomosaicProgram = config.photomosaic
+      mapShipwreck = config.shipwreck_mapper
       
       
       pubLasers.publish(toggleLasers)
       pubLength.publish(lengthProgram)
       pubPhotomosaic.publish(photomosaicProgram)
+      pubShipwreck.publish(mapShipwreck)
       
       config.length_finder = False
       config.photomosaic = False
+      config.shipwreck_mapper = False
     
       return config
       
