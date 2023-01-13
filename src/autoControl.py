98acb157c1b08ce045e9926ae46f2851806abb63
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2023-01-12 21:07:35 -0800

    Depth Hold GUI

--- src/autoControl.py
+++ src/autoControl.py
@@ -4,3 +4,25 @@ from std_msgs.msg import UInt8
 from dynamic_reconfigure.server import Server
 from copilot_interface.cfg import depthControlParamsConfig
 from copilot_interface.msg import autoControlData
+
+autoControl = autoControlData()
+
+def autoControlCallback(config, level):
+  #Toggle Depth Hold and select Target Depth
+  autoControl.dh_status = config.depthHoldToggle
+  autoControl.target_depth = config.targetDepth
+
+  #Update pid scalars
+  autoControl.p_scalar = config.pScalar
+  autoControl.i_scalar = config.iScalar
+  autoControl.d_scalar = config.dScalar
+  auto_control_pub.publish(autoControl)
+
+  return config
+
+if __name__  == "__main__":
+  rospy.init_node('DH_GUI')
+  auto_control_pub = rospy.Publisher('auto_control', autoControlData, queue_size=1) # Publisher to autoControl
+  server = Server(depthControlParamsConfig, autoControlCallback)
+  rospy.spin()
+  
